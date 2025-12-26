b0f5cb100fdfaa4e4fdc1749589e710f50c753c2
iamprashu <iamprashu.gs@gmail.com>
2025-12-26 23:17:55 +0530

    pages

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+  return (
+    <div className="w-screen h-screen ">
+      <div className="w-md h-md block mx-auto ">this is auth page</div>
+    </div>
+  );
+};
+
+export default Auth;
