995fa0fd7ab1e42bfc509f6e65a370c8a8d0f224
iamprashu <iamprashu.gs@gmail.com>
2025-12-28 20:39:05 +0530

    Main Router implimented properly with Public and Private routes

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PublicRoute from "../components/PublicRoute";
 import Login from "../pages/auth/Login";
+import PrivateRoutes from "../components/PrivateRoutes";
+import Home from "../pages/Home";
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* public routes if  */}
         <Route path="/" element={<PublicRoute />}>
           <Route index element={<Login />} />
         </Route>
 
-        <Route path="/app" element={<div>hello</div>} />
+        {/* private routes */}
+        <Route path="/app" element={<PrivateRoutes />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
