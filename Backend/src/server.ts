a44feec2b12e2769f1126da883238cf296cc1900
iamprashu <iamprashu.gs@gmail.com>
2025-12-30 11:56:20 +0530

    Setup prisma Client and ENV

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,17 +1,16 @@
 import express from "express";
-import "dotenv/config";
 import authRoutes from "./routers/Auth.router";
 import cors from "cors";
-
+import "dotenv/config.js";
 const app = express();
-const PORT = process.env.APP_PORT!;
+const PORT = process.env.PORT;
 
 if (!PORT) {
   throw new Error("Please provide application port");
 }
 
 const corsConfig = {
-  origin: ["http://localhost:5713/"],
+  origin: ["http://localhost:3000"],
 };
 
 // middlewares
